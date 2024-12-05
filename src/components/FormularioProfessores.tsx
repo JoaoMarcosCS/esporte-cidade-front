ad72bde1750e2828632d8b32be72d21c359b702c
Eric Moura <124516716+Wr3tchedTorch@users.noreply.github.com>
2024-12-05 12:07:30 -0300

    add campo de senha opcional da tela de cadastro de professor

--- src/components/FormularioProfessores.tsx
+++ src/components/FormularioProfessores.tsx
@@ -134,15 +134,15 @@ const FormularioProfessores = forwardRef<HTMLFormElement, Props>(
           <h2 className="font-bold text-3xl mb-4 mt-20">{professorEdicao ? "Editar Professor" : "Cadastrar Professor"}</h2>
           <div className="md:flex md:flex-wrap justify-start gap-x-10 gap-y-10">
             <section className="md:w-2/5 gap-10">
-              <Textbox value={formData.name} onChange={handleChange} name="name" label="Nome" iconPath="/icon/id.svg" placeholder="Insira o nome completo" type="text" />
+              <Textbox value={formData.name} onChange={handleChange} name="name" label="Nome" iconPath="/icon/id.svg" placeholder="Insira o nome completo" type="text" required={true} />
 
               <Datepicker label="Data de nascimento" name="birthday" value={formData.birthday} onChange={handleChange} iconPath="/icon/date.svg" />
 
-              <Textbox value={formData.password} onChange={handleChange} name="password" label="Senha" iconPath="/icon/id.svg" placeholder="Insira a senha" type="password" />
+              <Textbox value={formData.password} onChange={handleChange} name="password" label={professorEdicao ? "Nova senha (opcional)" : "Senha"} iconPath="/icon/id.svg" placeholder={professorEdicao ? "Nova senha (caso deseje alterar)." : "Insira a senha"} type="password" required={professorEdicao === null} />
 
-              <Textbox value={formData.phone} onChange={handleChange} name="phone" label="Telefone" iconPath="/icon/phone.svg" placeholder="(__)_____-____" type="text" />
+              <Textbox value={formData.phone} onChange={handleChange} name="phone" label="Telefone" iconPath="/icon/phone.svg" placeholder="(__)_____-____" type="text" required={true} />
 
-              <Textbox value={formData.photo_url} onChange={handleChange} name="photo_url" label="URL da foto" iconPath="/icon/mailbox.svg" placeholder="Insira o link da foto" type="url" />
+              <Textbox value={formData.photo_url} onChange={handleChange} name="photo_url" label="URL da foto" iconPath="/icon/mailbox.svg" placeholder="Insira o link da foto" type="url" required={true} />
 
               <Dropdown label="Modalidade" name="modality" onChange={handleChange} value={formData.modality != null ? formData.modality.id : 0} iconPath="/icon/soccer.svg">
                 <option value="">Selecione a modalidade</option>
@@ -155,13 +155,13 @@ const FormularioProfessores = forwardRef<HTMLFormElement, Props>(
             </section>
 
             <section className="md:w-2/5">
-              <Textbox value={formData.cpf} onChange={handleChange} name="cpf" label="CPF" iconPath="/icon/id.svg" placeholder="Insira o CPF" type="text" />
+              <Textbox value={formData.cpf} onChange={handleChange} name="cpf" label="CPF" iconPath="/icon/id.svg" placeholder="Insira o CPF" type="text" required={true} />
 
-              <Textbox value={formData.rg} onChange={handleChange} name="rg" label="RG" iconPath="/icon/id.svg" placeholder="Insira o RG" type="text" />
+              <Textbox value={formData.rg} onChange={handleChange} name="rg" label="RG" iconPath="/icon/id.svg" placeholder="Insira o RG" type="text" required={true} />
 
-              <Textbox value={formData.email} onChange={handleChange} name="email" label="E-mail do professor" iconPath="/icon/mail.svg" placeholder="Insira o e-mail" type="text" />
+              <Textbox value={formData.email} onChange={handleChange} name="email" label="E-mail do professor" iconPath="/icon/mail.svg" placeholder="Insira o e-mail" type="text" required={true} />
 
-              <Textbox value={formData.about} onChange={handleChange} name="about" label="Sobre o professor" iconPath="/icon/id.svg" placeholder="Descreva o professor" type="text" multiline={true} />
+              <Textbox value={formData.about} onChange={handleChange} name="about" label="Sobre o professor" iconPath="/icon/id.svg" placeholder="Descreva o professor" type="text" multiline={true} required={true} />
             </section >
 
             <div className="w-full flex justify-between">
