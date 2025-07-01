b8cd9eb8c53ca7ae0f9d9a7e36a7533d54dcf538
gabebastarz1 <gabrielbastarz.tggt@gmail.com>
2025-07-01 14:37:08 -0300

    .env fix

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3002/api/',
+  baseURL: process.env.REACT_APP_API_URL,
 });
 
 // Add this interceptor to include the token in every request
